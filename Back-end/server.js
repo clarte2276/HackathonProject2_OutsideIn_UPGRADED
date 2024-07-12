568892d36d6dfc436af77ec45b45f8872672ecb0
ssungcoding <tjdgus2532@gmail.com>
2024-07-12 14:02:08 +0900

    파일 다듬기 시작

--- Back-end/server.js
+++ Back-end/server.js
@@ -6,8 +6,47 @@ const session = require("express-session");
 const MySQLStore = require("express-mysql-session")(session);
 const bcrypt = require("bcrypt");
 const db_config = require("./config/db_config.json");
-const cors = require("cors"); 
 
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+
+// MySQL 세션 스토어 옵션
+const sessionStoreOptions = {
+  host: db_config.host,
+  port: db_config.port,
+  user: db_config.user,
+  password: db_config.password,
+  database: db_config.database,
+};
+
+const sessionStore = new MySQLStore(sessionStoreOptions);
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: db_config.host,
+  user: db_config.user,
+  password: db_config.password,
+  database: db_config.database,
+  port: db_config.port,
+  debug: false,
+});
+
+// URL을 인코딩하는 코드
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(
+  session({
+    key: "session_cookie_name",
+    secret: "your_secret_key",
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 24 * 60 * 60 * 1000 }, // 24시간
+  })
+);
+
+const indexRoutes = require("./function/index");
+const mypageRoutes = require("./function/mypage");
+const loginRoutes = require("./function/login");
