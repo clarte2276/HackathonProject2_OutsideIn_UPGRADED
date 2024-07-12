b70c586fbbaca471c3cf3567cfc33cce43ccb4bd
ssungcoding <tjdgus2532@gmail.com>
2024-07-12 23:20:44 +0900

    은이 공부용

--- Back-end/function/mypage.js
+++ Back-end/function/mypage.js
@@ -5,11 +5,9 @@ const bcrypt = require("bcrypt");
 const app = express();
 const router = express.Router();
 
-// /mypage 접속했을 때 다음과 같은 일을 하세요
-app.get("/mypage", (req, res) => {
-  console.log("Here is index page");
+router.get("/", (res, req) => {
+  console.log("here is mypage tab");
 });
-
 // /mypage/~~ 에 접속했을 때 다음과 같은 일을 하세요
 router.get("/~~", (req, res) => {
   console.log("Here is mypage/~~~ page");
