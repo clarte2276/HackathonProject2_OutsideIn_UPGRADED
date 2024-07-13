c344d4358bcd12671b9f3d5cd49d11a67731b545
on22y <lucy14lee@gmail.com>
2024-07-14 00:53:53 +0900

    hospitalmain

--- front-end/src/components/hospital/Hospital.js
+++ front-end/src/components/hospital/Hospital.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import arrowImg from '../images/arrow.png';
 
 function Hospital() {
   return (
-    <div>
-      <h1>Hospital page</h1>
+    <div className="hospitalmainPage">
+      <div className="hospitalHeader">
+        <h1>
+          믿을 수 있는 병원!
+          <br />
+          신뢰할 수 있는 의사!
+        </h1>
+      </div>
+      <div className="doctorTap">
+        <h2>병원A / 김성현 원장</h2>
+        <p>서울특별시 중구 필동로</p>
+        <Link to="/hospitalA">
+          <img className="arrowIcon" src={arrowImg} alt="병원A로 이동" />
+        </Link>
+      </div>
+      <div className="doctorTap">
+        <h2>병원B / 조명기 원장</h2>
+        <p>경기도 수원시</p>
+        <Link to="/HospitalB">
+          <img className="arrowIcon" src={arrowImg} alt="병원B로 이동" />
+        </Link>
+      </div>
+      <div className="doctorTap">
+        <h2>병원C / 이예령 원장</h2>
+        <p>평화누리자치도</p>
+        <Link to="/HospitalC">
+          <img className="arrowIcon" src={arrowImg} alt="병원C로 이동" />
+        </Link>
+      </div>
     </div>
   );
 }
