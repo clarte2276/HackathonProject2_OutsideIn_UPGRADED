75a7ea5e985c927cb24b624422e808f84d7e0d8d
on22y <lucy14lee@gmail.com>
2024-07-15 00:25:27 +0900

    성공한 것.

--- Back-end/function/process.js
+++ Back-end/function/process.js
@@ -1,52 +1,32 @@
 // /process를 통해 server에 있는 정보를 줄 때 이 파일을 통해 주게됨
-const express = require("express");
-const mysql = require("mysql");
-const bcrypt = require("bcrypt");
+const express = require('express');
+const mysql = require('mysql');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 
-router.get("/check-login", (req, res) => {
-  console.log("/check-login 호출됨"); // 디버깅용 로그asdas
+router.get('/check-login', (req, res) => {
+  console.log('/check-login 호출됨'); // 디버깅용 로그asdas
   if (req.session.user) {
-    console.log("로그인 상태 확인됨"); // 디버깅용 로그
+    console.log('로그인 상태 확인됨'); // 디버깅용 로그
     res.json({ loggedIn: true, user: req.session.user });
   } else {
-    console.log("로그인 상태 아님"); // 디버깅용 로그
+    console.log('로그인 상태 아님'); // 디버깅용 로그
     res.json({ loggedIn: false });
   }
 });
 
-router.post("/hospital", (req, res) => {
-  if (!req.session.user) {
-    return res.status(401).send("로그인이 필요합니다.");
-  }
-  const currentUserID = req.session.user.id;
-  pool.query(
-    "SELECT id, nickname FROM users WHERE id != ?",
-    [currentUserID],
-    (error, results) => {
-      if (error) {
-        console.error(error);
-        res.status(500).send("서버 오류");
-      } else {
-        console.log(results); // 응답 데이터 확인을 위한 로그 추가
-        res.json(results);
-      }
-    }
-  );
-});
-
 // 로그아웃 구현
-router.post("/logout", (req, res) => {
+router.post('/logout', (req, res) => {
   req.session.destroy((err) => {
     if (err) {
-      console.log("세션 종료 중 오류 발생:", err);
-      return res.status(500).send("로그아웃 중 오류가 발생했습니다.");
+      console.log('세션 종료 중 오류 발생:', err);
+      return res.status(500).send('로그아웃 중 오류가 발생했습니다.');
     }
-    res.clearCookie("session_cookie_name");
+    res.clearCookie('session_cookie_name');
     return res.status(200).send();
   });
 });
 
 //app과 router 연동
-router.use("/process", router);
+router.use('/process', router);
 module.exports = router;
--- Back-end/server.js
+++ Back-end/server.js
@@ -1,12 +1,12 @@
 // server.js
-const express = require("express");
-const mysql = require("mysql");
-const path = require("path");
-const session = require("express-session");
-const MySQLStore = require("express-mysql-session")(session);
-const db_config = require("./config/db_config.json");
+const express = require('express');
+const mysql = require('mysql');
+const path = require('path');
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+const db_config = require('./config/db_config.json');
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
 
 // MySQL 세션 스토어 옵션
 const sessionStoreOptions = {
@@ -35,8 +35,8 @@ app.use(express.json());
 app.use(cors());
 app.use(
   session({
-    key: "session_cookie_name",
-    secret: "your_secret_key",
+    key: 'session_cookie_name',
+    secret: 'your_secret_key',
     store: sessionStore,
     resave: false,
     saveUninitialized: false,
@@ -45,36 +45,32 @@ app.use(
 );
 
 // 정적 파일 제공
-app.use(express.static(path.join(__dirname, "../Front-end/build")));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, '../Front-end/build')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //js파일 연동
-const mypageRoutes = require("./function/mypage");
-const loginRoutes = require("./function/login");
-const processRoutes = require("./function/process");
-const communityRoutes = require("./function/community");
-const chatsRoutes = require("./function/chats");
-const commentsRoutes = require("./function/comments");
-const reservation = require("./function/reservation");
-const kakaoRoutes = require("./function/kakao_login");
-const hoschatRoutes = require("./function/hoschat");
+const mypageRoutes = require('./function/mypage');
+const loginRoutes = require('./function/login');
+const processRoutes = require('./function/process');
+const communityRoutes = require('./function/community');
+const chatsRoutes = require('./function/chats');
+const commentsRoutes = require('./function/comments');
+const kakaoRoutes = require('./function/kakao_login');
 
-app.use("/", mypageRoutes);
-app.use("/", loginRoutes);
-app.use("/", processRoutes);
-app.use("/", communityRoutes);
-app.use("/", chatsRoutes);
-app.use("/", commentsRoutes);
-app.use("/", reservation);
-app.use("/", kakaoRoutes);
-app.use("/", hoschatRoutes);
+app.use('/', mypageRoutes);
+app.use('/', loginRoutes);
+app.use('/', processRoutes);
+app.use('/', communityRoutes);
+app.use('/', chatsRoutes);
+app.use('/', commentsRoutes);
+app.use('/', kakaoRoutes);
 
 //모든 요청은 build/index.html로
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../Front-end/build", "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../Front-end/build', 'index.html'));
 });
 
 // 서버 시작
 app.listen(3000, () => {
-  console.log("서버가 3000 포트에서 실행 중입니다.");
+  console.log('서버가 3000 포트에서 실행 중입니다.');
 });
